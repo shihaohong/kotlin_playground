1effbd66b96dc722314495cffbabd16de23d9896
Shi Hao Hong <shihaohong94@gmail.com>
2021-07-27 14:47:21 +0800

    More code to dice.kt -- Kotlin classes

--- dice.kt
+++ dice.kt
@@ -1,5 +1,17 @@
 fun main() {
     val diceRange = 1..6
     val randomNumber = diceRange.random()
-    print("Random number: ${randomNumber}")
+    println("Random number: ${randomNumber}")
+
+    val myFirstDice = Dice(6)
+    println("${myFirstDice.numSides}-sided dice roll result is: ${myFirstDice.roll()}")
+    val mySecondDice = Dice(20)
+    println("${mySecondDice.numSides}-sided dice roll result is: ${mySecondDice.roll()}")
+
+}
+
+class Dice (val numSides: Int){
+    fun roll() : Int {
+        return (1..numSides).random()
+    }
 }
